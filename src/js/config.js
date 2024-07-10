6fadd7c6781251221eeff05ecf22ee91396dc46f
SATYAM KUMAR <futurebillionare143@gmail.com>
2024-07-10 15:48:03 +0530

    Project Completed

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '77e11e49-64aa-4c9a-b250-f448380c7847';
+export const MODAL_CLOSE_SEC = 2.5;
